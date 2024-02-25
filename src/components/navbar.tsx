17ee9b55fd454bb1d83d93a82a52f73f0f1563a9
InfinitePower563 <nathan-lee1111@outlook.com>
2024-02-24 21:11:46 -0600

    Run away from my problems (aka issue #1 )

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,18 @@
-import Image from "next/image";
+'use client';
+
+import Img from "next/image";
 import logo from "@/assets/logo.png";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Link from "next/link";
+
 
 export default function NavBar() {
     return (
         <nav className="topnav" id="navbar">
-            <Link href="/">
-                <Image src={logo} alt={"Home"} className={"App-logo"}/>
+            <Link href={"/"}>
+                <Img src={logo} alt={"Home"} className={"App-logo"} priority={true}/>
             </Link>
+
             {/*
                 <a href="/math/math.html">Mathematics</a>
                 <a href="/sci/sci.html">Science</a>
